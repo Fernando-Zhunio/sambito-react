f471d3d1d582ee20682beecab904c6663d6ae614
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2022-02-12 18:44:10 -0500

    change

--- layout/DefaultLayout.tsx
+++ layout/DefaultLayout.tsx
@@ -2,11 +2,35 @@ import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarFooter } fro
 import Header from '../layout/Header';
 import 'react-pro-sidebar/dist/css/styles.css';
 import Link from 'next/link';
+
 import { IoMdKeypad, IoMdListBox, IoMdPeople, IoMdArrowForward, IoMdList } from 'react-icons/io';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { getSession } from 'next-auth/client';
 
+interface ISidebarTitle {
+    is: "header";
+    icon: any;
+    title: string;
+}
+interface ISidebarMenu {
+    is: "menu";
+    menu: (ISidebarItem | ISidebarSubmenu)[];
+}
+
+interface ISidebarItem {
+    is: "item";
+    icon: any;
+    title: string;
+    to: string;
+}
+
+interface ISidebarSubmenu {
+    is: "submenu";
+    icon: any;
+    title: string;
+    menu: (ISidebarItem)[];
+}
 
 function DefaultLayout({ children }) {
     const router = useRouter();
@@ -24,22 +48,22 @@ function DefaultLayout({ children }) {
     };
 
     const [menus, setMenus] = useState([]);
-    
 
-   const obtenerDatos = async () => {
-    const session = await getSession();
-    let rol = session.user.image;
-    const url = process.env.URL+"/menu";
 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ rol: rol })
-    };
-    fetch(url, requestOptions)
-        .then(response => response.json())
-        .then(data => setMenus(data.menus));
-  }
+    const obtenerDatos = async () => {
+        const session = await getSession();
+        let rol = session.user.image;
+        const url = process.env.URL + "/menu";
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ rol: rol })
+        };
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(data => setMenus(data.menus));
+    }
 
     useEffect(() => {
         window.addEventListener("resize", handleIsMobileChange);
@@ -58,21 +82,21 @@ function DefaultLayout({ children }) {
         }
     }
 
-  //  console.log(menus)
+    //  console.log(menus)
 
-    const lateral = menus.map(function(obj){
+    const lateral: ISidebarItem[] = menus.map(function (obj) {
         let rObj = {};
         rObj["is"] = obj.tipo;
-        rObj["is"] = obj.tipo;
-        rObj["is"] = obj.tipo;
+        // rObj["is"] = obj.tipo;
+        // rObj["is"] = obj.tipo;
         rObj["icon"] = obj.icono;
         rObj["title"] = obj.nombre_menu;
         rObj["to"] = obj.url;
-        return rObj;
-     });
-//    console.log(lateral);
+        return rObj as ISidebarItem;
+    });
+    //    console.log(lateral);
 
-    const itemDashboard = [
+    const itemDashboard: (ISidebarTitle | ISidebarMenu)[] = [
         {
             is: "header",
             icon: <IoMdListBox />,
@@ -80,7 +104,56 @@ function DefaultLayout({ children }) {
         },
         {
             is: "menu",
-            menu: lateral,
+            menu: [
+                {
+                    is: "item",
+                    icon: <IoMdKeypad />,
+                    title: 'Requisitos Auditables',
+                    to: '/auditable-requirements',
+                },
+                {
+                    is: "submenu",
+                    icon: <IoMdKeypad />,
+                    title: 'Requisitos Auditables',
+                    menu: [
+                        {
+                            is: "item",
+                            icon: <IoMdKeypad />,
+                            title: 'Crud',
+                            to: '/crud',
+                        }
+                    ]
+                }
+            ],
+        },
+        {
+            is: "header",
+            icon: <IoMdListBox />,
+            title: 'Requisitos',
+        },
+        {
+            is: "menu",
+            menu: [
+                {
+                    is: "item",
+                    icon: <IoMdKeypad />,
+                    title: 'Requisitos',
+                    to: '/auditable-requirements',
+                },
+                {
+                    is: "submenu",
+                    icon: <IoMdKeypad />,
+                    title: 'Requisitos Auditables',
+                    menu: [
+                        {
+                            is: "item",
+                            icon: <IoMdKeypad />,
+                            title: 'Crud',
+                            to: '/crud',
+                        }
+                    ]
+                }
+            ],
         }
     ]
 
@@ -100,10 +173,21 @@ function DefaultLayout({ children }) {
                             menu.map((item, index1) => {
                                 const menu = item;
                                 if (menu.is == "item") {
-                                    return (<MenuItem key={index + '-' + index1} className={router.pathname == item.to ? "selected" : ""} icon={<div dangerouslySetInnerHTML={{__html: menu.icon}}></div>}>
+                                    return (<MenuItem key={index + '-' + index1} className={router.pathname == item.to ? "selected" : ""} icon={menu.icon}>
                                         <Link href={menu.to}>{menu.title}</Link>
                                     </MenuItem>)
+                                } else if (menu.is == "submenu") {
+                                    return (<SubMenu key={index + '-' + index1} icon={menu.icon} title={menu.title} >
+                                        {
+                                            menu.menu.map((item1, index2) => {
+                                                return (<MenuItem key={index + '-1' + index2} className={router.pathname == item1.to ? "selected" : ""} icon={item1.icon}>
+                                                <Link href={item1.to}>{item1.title}</Link>
+                                            </MenuItem>)
+                                            })
+                                        }
+                                        </SubMenu>)
                                 }
+
                             })
                         }
                     </Menu>
